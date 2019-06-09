fb92f70f702ce89ce4f6c004729f3df644f126a0
Сергей <nyos@rambler.ru>
2019-06-10 00:57:12 +0500

    no message

--- class.php
+++ class.php
@@ -9,7 +9,7 @@ namespace Nyos\mod;
 if (!defined('IN_NYOS_PROJECT'))
     throw new \Exception('Сработала защита от розовых хакеров, обратитесь к администрратору');
 
-class mod_class {
+class JobDesc {
 
     public static $dir_img_server = false;
 
