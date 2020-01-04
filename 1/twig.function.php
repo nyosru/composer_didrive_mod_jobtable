7265796c170653f9481a4a685d527a1b7035f5ee
Сергей <nyos@rambler.ru>
2020-01-04 14:42:51 +0500

    1.2.39 добавил сортировку по должностям, в мемкеш пишем массив должностей (функция в твиге)

--- 1/twig.function.php
+++ 1/twig.function.php
@@ -1,13 +1,33 @@
 <?php
 
-
-
 /**
-определение функций для TWIG
+  определение функций для TWIG
  */
 //creatSecret
-
 // $function = new Twig_SimpleFunction('creatSecret', function ( string $text ) {
 //    return \Nyos\Nyos::creatSecret($text);
 // });
 // $twig->addFunction($function);
+
+$function = new Twig_SimpleFunction('jobdesc__get_dolgnosti', function ( $db, $mod = '061.dolgnost' ) {
+
+    // \f\Cash::deleteKeyPoFilter( ['dolgnosti'] );
+    $dolgn = \f\Cash::getVar('dolgnosti');
+
+    // если не пусто то вернули из мемкеша
+    if (!empty($dolgn)) {
+        // \f\pa($dolgn,2,'','$e');
+        $dolgn['cash'] = 'da';
+    }
+    // если пусто то достаём по новой и записываем в мемкеш
+    else {
+        $dolgn['data'] = $dolgn['sort'] = \Nyos\mod\items::get($db, $mod);
+        usort($dolgn['sort'], "\\f\\sort_ar_sort_desc");
+        \f\Cash::setVar('dolgnosti', $dolgn, 0 );
+        $dolgn['cash'] = 'no';
+        // \f\pa($dolgn,2,'','$e2');
+    }
+
+    return $dolgn;
+});
+$twig->addFunction($function);
---
+++ README.md
@@ -0,0 +1,6 @@
+# composer_didrive_mod_job_load_time_expectation
+работа с загрузкой и обработкой времени ожидания
+
+храним ключи в мемкеш
+dolgnosti - [ 'data' => [ dolgnosti ], 'sort' => [ dolg отсортированные по полю сортировки ] ]
+в twig функции > jobdesc__get_dolgnosti
