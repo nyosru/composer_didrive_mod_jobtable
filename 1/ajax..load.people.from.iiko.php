e54e045eb5160fb5d7724ca206abd14b6a4287be
Сергей <nyos@rambler.ru>
2020-04-22 03:26:01 +0500

    1.9.0

--- 1/ajax..load.people.from.iiko.php
+++ 1/ajax..load.people.from.iiko.php
@@ -16,18 +16,21 @@ define('IN_NYOS_PROJECT', true);
 require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/all/ajax.start.php';
 
-
 \f\timer_start(1);
-
-
 
 
 // если нужно не обращать внимания на кеш
 if (!empty($_GET['no_load_cash']))
     \Nyos\api\Iiko::$cash = false;
 
+// getConfigDbIiko
+
+\Nyos\api\Iiko::getConfigDbIiko();
 $e = \Nyos\api\Iiko::loadIikoPeople();
-// \f\pa($e, 2, '', ' результат загрузки');
+
+// die('<br/>#'.__LINE__);
+\f\pa($e, 2, '', ' результат загрузки');
+// die('<br/>#'.__LINE__);
 //                // если кеш есть
 //                if (isset($e['file_cash_est']) && $e['file_cash_est'] == 'da') {
 //                    echo '<br/>#' . __LINE__;
@@ -38,17 +41,24 @@ $e = \Nyos\api\Iiko::loadIikoPeople();
 //                }
 
 $e2 = \Nyos\api\Iiko::saveIikoPeople($db, $e['data']);
-// \f\pa($e2, 2, '', ' результат выполнения загрузки и проверки данных');
 
-$msg2 = '';
+\f\pa($e2, 2, '', ' результат выполнения загрузки и проверки данных');
 
-if ((!empty($e2['kolvo_new']) && is_numeric($e2['kolvo_new']) && $e2['kolvo_new'] > 0 ) || (!empty($e2['kolvo_edit_dop']) && is_numeric($e2['kolvo_edit_dop']) && $e2['kolvo_edit_dop'] > 0 )) {
+$msg2 = 'Добавлено пользователей: ' . $e2['new_items'] 
+        . PHP_EOL . 'Обновлено параметров: ' . $e2['new_dops_kolvo'];
 
-    $msg2 .= 'удаляем кеш данных так как были изменения в локальной базе даных' . PHP_EOL;
-}
+//die('<br/>#' . __LINE__);
+//
+//
+//
+//$msg2 = '';
+//
+//if ((!empty($e2['kolvo_new']) && is_numeric($e2['kolvo_new']) && $e2['kolvo_new'] > 0 ) || (!empty($e2['kolvo_edit_dop']) && is_numeric($e2['kolvo_edit_dop']) && $e2['kolvo_edit_dop'] > 0 )) {
+//
+//    $msg2 .= 'удаляем кеш данных так как были изменения в локальной базе даных' . PHP_EOL;
+//}
 
-$msg2 .= \f\timer_stop(1) . PHP_EOL;
-
+$msg2 .= PHP_EOL . 'комп время: ' . \f\timer_stop(1) . PHP_EOL;
 
 try {
 
@@ -57,13 +67,13 @@ try {
         // echo 'Послали отчёт об операции в телеграм';
         // $msg = 'Загрузили данные с айки по пользователям ( ' . sizeof($re) . ' записей )';
         $msg = 'Загрузили данные с айки по пользователям'
-                . PHP_EOL . 'записей ' . ( $e2['kolvo_in'] ?? '-' )
-                . PHP_EOL . 'новых сотрудников  ' . ( $e2['kolvo_new'] ?? '-' )
-                . PHP_EOL . 'изменённых доп параметров ' . ( $e2['kolvo_edit_dop'] ?? '-' )
+//                . PHP_EOL . 'записей ' . ( $e2['kolvo_in'] ?? '-' )
+//                . PHP_EOL . 'новых сотрудников  ' . ( $e2['kolvo_new'] ?? '-' )
+//                . PHP_EOL . 'изменённых доп параметров ' . ( $e2['kolvo_edit_dop'] ?? '-' )
                 . PHP_EOL . ( $msg2 ?? '' )
         ;
 
-        \Nyos\Msg::sendTelegramm($msg, null, 1);
+        \Nyos\Msg::sendTelegramm($msg, null, 2);
 
         if (file_exists(DR . DS . 'sites' . DS . \Nyos\Nyos::$folder_now . DS . 'config.php'))
             require_once DR . DS . 'sites' . DS . \Nyos\Nyos::$folder_now . DS . 'config.php';
